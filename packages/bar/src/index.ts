7c60014964b1d3d0a310bdbeb4c724b15207ca0a
reallifeliver <pms3300@gmail.com>
2020-09-17 16:58:36 +0900

    set for typescript

---
+++ packages/bar/src/index.ts
@@ -0,0 +1,3 @@
+import FOO from '@reallifeliver/foo'
+
+console.log(FOO)
---
+++ packages/foo/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var FOO = 'foo';
+exports.default = FOO;
+//# sourceMappingURL=index.js.map
